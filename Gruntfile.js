10cb62220e3ac3c49cf22e97d857d195e2938c04
kongtoonarmy <kongtoonarmy@hotmail.com>
2016-02-10 19:22:54 +0700

    Using 'grunt-contrib-watch' plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,6 +28,13 @@ module.exports = function(grunt) {
                     "css/style-less.css": "less/source.less"
                 }
             }
+        },
+
+        watch: {
+            less: {
+                files: ['less/*.less'],
+                tasks: ['less']
+            }
         }
 
     });
@@ -36,6 +43,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-uncss');
     grunt.loadNpmTasks('grunt-processhtml');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
  
     // Set default task
     grunt.registerTask('default', ['uncss', 'processhtml', 'less']);
